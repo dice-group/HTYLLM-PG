6c5a121eba67dfcd7c72b02122ed49d0459bdeb1
Luke Friedrichs <lukefriedrichs@gmail.com>
2025-04-30 16:50:39 +0200

    refactoring

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
-        'tensorflow==2.19.0',
-        'transformers'
-    ],
+    ]
+    ,
+
 )
