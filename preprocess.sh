1509ed60c012da06b6216a24a6beb6b8a6a85353
LckyLke <lukefriedrichs@gmail.com>
2025-05-11 09:42:45 +0200

    explicitly passing cpu count as otherwise just runs single cpu?

--- preprocess.sh
+++ preprocess.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=preprocess-data
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=128
 #SBATCH --time=12:00:00
 #SBATCH --partition=normal
 #SBATCH --mem=64GB
@@ -13,6 +13,6 @@
 source ~/miniconda3/bin/activate icebreaker
 
 # Run preprocessing on a single node
-srun --nodes=1 --ntasks=1 python src/preprocess.py --files "../../data/**/*.jsonl.gz" --tokenizer tokenizer
+srun --nodes=1 --ntasks=1 python src/preprocess.py --files "../../data/**/*.jsonl.gz" --tokenizer tokenizer --num_proc 32
 
 echo "Preprocessing complete!" 
