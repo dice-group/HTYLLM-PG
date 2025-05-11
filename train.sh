86733f5741377866e58ad9e1567594de6d143e14
LckyLke <lukefriedrichs@gmail.com>
2025-05-11 11:17:58 +0200

    bash trainig updated

--- train.sh
+++ train.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=multinode-example
-#SBATCH --nodes=16   
+#SBATCH --nodes=16
 #SBATCH --ntasks=16
 #SBATCH --cpus-per-task=4
-#SBATCH --time=48:00:00
+#SBATCH --time=12:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:a100:1
 #SBATCH --mem=64GB
-
+#SBATCH --account=hpc-prf-merlin
 
 nodes=( $( scontrol show hostnames $SLURM_JOB_NODELIST ) )
 nodes_array=($nodes)
