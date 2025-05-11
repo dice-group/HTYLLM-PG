b2bf4b6a24b00d60a6ff5a1dcc6a47d929609892
Sashreek Dhaimodkar <dhaimodkarsashreek@gmail.com>
2025-05-11 08:29:38 +0200

    Updated tokenizer.py

--- src/data/tokenizer.py
+++ src/data/tokenizer.py
@@ -8,8 +8,8 @@ from transformers import BertTokenizer
 import gzip
 import json
 
-#data_folder = "/scratch/hpc-prf-merlin/htyllm-pg/data/fineweb2_subset"
-data_folder = "src/data/fineweb2_subset"
+data_folder = "/scratch/hpc-prf-merlin/htyllm-pg/data/fineweb2_subset"
+#data_folder = "src/data/fineweb2_subset"
 tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-uncased')
 
 num_proc = 8
