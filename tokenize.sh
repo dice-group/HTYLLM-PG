84a61b2796af13ba616834f30aa1dc236432c2c6
LckyLke <lukefriedrichs@gmail.com>
2025-05-21 14:25:01 +0200

    debug

--- tokenize.sh
+++ tokenize.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-#SBATCH --job-name=tokenize-data
+#SBATCH --job-name=tokenize-data-moe
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --time=12:00:00
 #SBATCH --partition=normal
-#SBATCH --mem=64GB
+#SBATCH --mem=128GB
+#SBATCH --qos=express
 #SBATCH --account=hpc-prf-merlin
 
 # Activate the conda environment
 source ~/miniconda3/bin/activate icebreaker 
-
+echo "Tokenizing data..."
 # Run tokenization on a single node
-srun --nodes=1 --ntasks=1 python tokenizer/train_tokenizer.py --files_glob "/scratch/hpc-prf-merlin/htyllm-pg/luke/data/**/*.jsonl.gz" --output_dir tokenizer
+srun --nodes=1 --ntasks=1 python tokenizer/train_tokenizer.py --files_glob "/scratch/hpc-prf-merlin/htyllm-pg/luke/data/**/*.jsonl.gz" --output_dir tokenizer --vocab_size 50304
 
 echo "Tokenization complete!"
