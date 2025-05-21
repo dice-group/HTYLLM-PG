c9b3d12e27128d4f2de5c5e79d1c18e59b0e6c6a
LckyLke <lukefriedrichs@gmail.com>
2025-05-21 12:49:42 +0200

    fixed tokenize.sh

--- tokenize.sh
+++ tokenize.sh
@@ -11,9 +11,8 @@
 
 # Activate the conda environment
 source ~/miniconda3/bin/activate icebreaker 
-python tokenizer/train_tokenizer.py --files_glob "/scratch/hpc-prf-merlin/htyllm-pg/luke/data/**/*.jsonl.gz" --output_dir tokenizer
 
 # Run tokenization on a single node
-srun --nodes=1 --ntasks=1 
+srun --nodes=1 --ntasks=1 python tokenizer/train_tokenizer.py --files_glob "/scratch/hpc-prf-merlin/htyllm-pg/luke/data/**/*.jsonl.gz" --output_dir tokenizer
 
 echo "Tokenization complete!"
