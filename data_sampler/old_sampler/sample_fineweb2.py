b806c18576ddd436b2f8a94ed9a717042b9af7d6
Joel Dag <joeldag6@gmail.com>
2025-07-12 10:30:00 +0200

    added lang codes to old sampler

--- data_sampler/old_sampler/sample_fineweb2.py
+++ data_sampler/old_sampler/sample_fineweb2.py
@@ -2,11 +2,48 @@ from multiprocessing import Pool, freeze_support
 from datatrove.executor import LocalPipelineExecutor
 from datatrove.pipeline.readers import ParquetReader
 from datatrove.pipeline.writers import JsonlWriter
-from language_codes import language_codes, belebele_languages
 import os
 
-OUTPUT_DIR = "/data/fineweb2_subset_belebele"
+OUTPUT_DIR = "/data/adapter_fineweb2_subset/niger_congo" #Adapt
 os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+language_codes = [ # 2Gb data -> 800.000.000 TOkens
+        "swh_Latn", #1.3gb -> beleble_swh_Latn 
+        "zul_Latn", # 182mb
+        
+        "nso_Latn", # 8mb -> belebele_nso_Latn
+        "tsn_Latn", # 9mb -> beleble_tsn_Latn
+        "sot_Latn", # 127mb ->
+        
+        "yor_Latn",# 96mb -
+        "ibo_Latn", # 140mb
+        
+        "ewe_Latn", # 3mb -> no eval
+        "fon_Latn", #2mb -> no eval
+    ]
+
+lang_code_2 = [
+    "beng_Beng", #20gb ->  5gb 
+    "asm_Beng",  # 334mb -> all
+]
+
+lang_code_2 = [
+    "kaz_Cyrl", # 6,21gb
+    "khk_Cyrl", # 2,5gb
+]
+
+lang_code_3 = [
+    "amh_Ethi", # 530mb
+    "tir_Ethi", # 141mb
+]
+
+lang_code_4 = [
+	"ars_Arab", #1.81GB
+	"apc_Arab",
+	"arb_Arab", #94.52GB
+	"acm_Arab",
+	"arz_Arab"
+]
 
 def run_executor(lang):
     if lang not in language_codes:
@@ -17,19 +54,18 @@ def run_executor(lang):
 
     executor = LocalPipelineExecutor(
         pipeline=[
-            ParquetReader(reader_path, limit=20000),
+            ParquetReader(reader_path),
             JsonlWriter(output_path)
         ],
-        tasks=3
+        tasks=9
     )
     executor.run()
 
 def main():
-    max_parallel_languages = 5  # Adjust this to control parallelism
-    languages_to_process = [lang for lang in belebele_languages if lang in language_codes]
+    max_parallel_languages = 9  # Adjust this to control parallelism
 
     with Pool(processes=max_parallel_languages) as pool:
-        pool.map(run_executor, languages_to_process)
+        pool.map(run_executor, language_codes)
 
 if __name__ == '__main__':
     freeze_support()
