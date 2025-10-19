9f82118af72e0904705691b1fdca9d5355a59e83
joeld <joeldag6@gmail.com>
2025-10-19 18:24:23 +0200

    added adapter approach files in seperate file only

--- language_adapters/scripts/extend_tokenizer/oov_corpus_validation.sh
+++
@@ -1,4 +0,0 @@
-python oov_corpus_validation.py \
-    --model  mistralai/Mistral-7B-v0.3 \
-    --data_dir /data/adapter_fineweb2_subset/arabic/ \
-    --max_lines 10000
--- language_adapters/scripts/obsolete/train_lora_gemma.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export CUDA_VISIBLE_DEVICES=0
-python train_lora_gemma.py
