ebdb1424a6fc0790e87ca3682ec382c4d089f10d
Luke Friedrichs <LckyLke>
2025-11-08 23:07:14 +0100

    added modules in slurm job

--- htyllm-pg/slurm/train_test.sh
+++ htyllm-pg/slurm/train_test.sh
@@ -14,6 +14,10 @@ set -e
 source ~/.bashrc
 conda activate moe
 
+module load system/CUDA/12.6.0
+module load compiler/GCCcore/12.3.0  
+
+
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 export NCCL_DEBUG=INFO
 
