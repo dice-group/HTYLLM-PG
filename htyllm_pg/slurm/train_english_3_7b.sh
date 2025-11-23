7b078f8db91b9c0b5f49bc885d58a0c73bcdf79b
Luke Friedrichs <lukefriedrichs@gmail.com>
2025-11-23 23:41:25 +0100

    testing ep

--- htyllm_pg/slurm/train_english_3_7b.sh
+++ htyllm_pg/slurm/train_english_3_7b.sh
@@ -73,5 +73,6 @@ srun --ntasks=${SLURM_NNODES} --ntasks-per-node=1 bash -c '
       --topany-gating-impl "sparse" \
       --use-gradient-checkpointing \
       --use-flash-attention \
+      --ep-size 16 \
       --data-dir /scratch/hpc-prf-merlin/luke/tokenized_english_data
 '
