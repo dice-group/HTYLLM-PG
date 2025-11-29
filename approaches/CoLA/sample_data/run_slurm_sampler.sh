8879deeca072d7a847fb7e0c372ac90acf6205d9
JoelDag <joeldag6@gmail.com>
2025-11-29 17:40:25 +0100

    minor adaptions

--- approaches/CoLA/sample_data/run_slurm_sampler.sh
+++ approaches/CoLA/sample_data/run_slurm_sampler.sh
@@ -5,14 +5,15 @@
 #SBATCH --mem=20G
 #SBATCH --time=20:00:00
 #SBATCH --output=logs/%x_%A_%a.out
+#SBATCH --partition=normal
 
 set -euo pipefail
 
 PLAN_CSV="/scratch/hpc-prf-merlin/joel/HTYLLM-PG/approaches/CoLA/data_prep/processed_artifacts/filtered_languages.csv"
 MAX_SAMPLE="100_000"
-OUTPUT_DIR="/scratch/hpc-prf-merlin/project_data/moe_study/adapter_dataset"
+OUTPUT_DIR="/scratch/hpc-prf-merlin/project_data/moe_study/adapter_dataset/inital_samples_datatrove"
 
 mkdir -p "${OUTPUT_DIR}"
 mkdir -p "logs"
 
-srun python sample_data/sample_generator_datatrove.py "${PLAN_CSV}" "${MAX_SAMPLE}" "${OUTPUT_DIR}"
+srun python sample_generator_datatrove.py "${PLAN_CSV}" "${MAX_SAMPLE}" "${OUTPUT_DIR}"
