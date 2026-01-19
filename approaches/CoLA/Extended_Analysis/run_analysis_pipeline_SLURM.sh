d60a91b73fb10a01553bc385654300dc5c7b1e71
shreekcode <dhaimodkarsashreek@gmail.com>
2026-01-19 10:35:40 +0100

    Update slurm script

--- approaches/CoLA/Extended_Analysis/run_analysis_pipeline_SLURM.sh
+++ approaches/CoLA/Extended_Analysis/run_analysis_pipeline_SLURM.sh
@@ -49,10 +49,10 @@ python -c "import torch; print('CUDA available:', torch.cuda.is_available()); pr
 
 # Configuration from command line or defaults
 BASE_MODEL="meta-llama/Llama-3.1-8B"
-CHECKPOINT="/scratch/hpc-prf-merlin/project_data/moe_study/multilingual_ablation_200_lang_cola/tier200_10_percent/lora_lora-baseline_20260108_054502/checkpoint-50000_adapter"
+CHECKPOINT="/scratch/hpc-prf-merlin/project_data/moe_study/multilingual_ablation_200_lang_cola/tier200_10_percent/cola_colaexp-hard_20260108_054502/checkpoint-60000_adapter"
 VALIDATION_DATA="/scratch/hpc-prf-merlin/project_data/moe_study/fw_samples/sharded_samples"
-LANGUAGES="english,rus_Cyrl,spa_Latn,fas_Arab,fin_Latn,hin_Deva,slv_Latn,ekk_Latn,zsm_Latn,srp_Cyrl,kat_Geor,tel_Telu"
-NUM_SEQUENCES="100"
+LANGUAGES="wbm_Latn,tel_Telu,fas_Arab,crl_Cans,abk_Cyrl,kru_Deva,ksw_Mymr,ekk_Latn,lao_Laoo,uzs_Arab,srp_Cyrl,kat_Geor,ory_Orya,hbo_Hebr,mhr_Cyrl,mni_Beng,moc_Latn,toc_Latn,agg_Latn,amr_Latn,sat_Olck,esi_Latn,fuv_Latn,tzm_Tfng,enx_Latn,gcf_Latn,pnt_Grek,pui_Latn,naq_Latn,kxm_Thai,guu_Latn,sah_Cyrl,spm_Latn,wuu_Hani,nas_Latn,dzo_Tibt,yuj_Latn,gnw_Latn,dty_Deva,jbo_Latn,wsg_Telu,nwx_Deva,spa_Latn,poi_Latn,crx_Latn,xav_Latn,tcy_Knda,gbi_Latn,mej_Latn,hto_Latn,gym_Latn,kaq_Latn,tat_Latn,dik_Latn,naf_Latn,liv_Latn,avt_Latn,miq_Latn,orv_Cyrl,azg_Latn,caa_Latn,jiv_Latn,asm_Beng,tig_Ethi,cpu_Latn,syc_Syrc,tkr_Cyrl,zsm_Latn,lsi_Latn,xal_Cyrl,tel_Latn,hin_Deva,dhg_Latn,cho_Latn,nab_Latn,ium_Latn,bon_Latn,ayo_Latn,cbi_Latn,ncj_Latn,ake_Latn,xnn_Latn,auc_Latn,ang_Latn,quz_Latn,emp_Latn,fin_Latn,guh_Latn,gum_Latn,jaa_Latn,sdh_Arab,wlv_Latn,crk_Latn,knv_Latn,tuo_Latn,slv_Latn,iws_Latn,arz_Arab,hae_Latn,myb_Latn,bug_Latn,koi_Cyrl,aey_Latn,atd_Latn,srd_Latn,wmt_Latn,gqr_Latn,kyf_Latn,sma_Latn,ige_Latn,ctp_Latn,mti_Latn,otw_Latn,xmm_Latn,chq_Latn,lbb_Latn,azj_Cyrl,csy_Latn,pxm_Latn,mse_Latn,ots_Latn,kxw_Latn,lob_Latn,njo_Latn,yua_Latn,kgk_Latn,hig_Latn,kyu_Latn,rtm_Latn,ile_Latn,hin_Latn,sbd_Latn,mww_Latn,hch_Latn,maf_Latn,snd_Deva,tee_Latn,toi_Latn,yut_Latn,swp_Latn,mqj_Latn,ubu_Latn,rus_Cyrl,nin_Latn,ksc_Latn,keo_Latn,esu_Latn,amk_Latn,bqp_Latn,laj_Latn,ibg_Latn,kck_Latn,poy_Latn,tay_Latn,rmn_Latn,cym_Latn,ojb_Cans,bgs_Latn,oku_Latn,mcb_Latn,cub_Latn,whg_Latn,bov_Latn,prf_Latn,hnn_Latn,lgl_Latn,kpw_Latn,dnj_Latn,cbr_Latn,jav_Latn,nba_Latn,gso_Latn,ntp_Latn,gng_Latn,prg_Latn,gwr_Latn,gmv_Latn,cpc_Latn,nuj_Latn,pma_Latn,acn_Latn,nii_Latn,rgu_Latn,zpq_Latn,mck_Latn,uvh_Latn,wew_Latn,bbr_Latn,bwq_Latn,gej_Latn,pam_Latn,nya_Latn,tso_Latn,mau_Latn,tue_Latn,bku_Latn,mie_Latn,ogo_Latn,ben_Latn,english"
+NUM_SEQUENCES="10000"
 BATCH_SIZE="16"
 # Note: --adapter_type, --num_layers, --num_experts are auto-detected from adapter_config.json
 
@@ -97,7 +97,9 @@ srun python tool/analyze_expert_routing.py \
     --test_data "$DATA_DIR" \
     --output "$OUTPUT_DIR" \
     --batch_size "$BATCH_SIZE" \
+    --max_sequences 100\
     --device cuda \
+    --use_language_ids
     2>&1 | tee "$LOGS_DIR/step2_analyze_${SLURM_JOB_ID}.log"
 echo ""
 
